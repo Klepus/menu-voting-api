03dd8645cdd5215d83ea3d9268b088970fed3276
Aleks Klepinin <aleks.klepinin@gmail.com>
2021-03-31 15:39:21 +0300

    Add menu service tests

--- src/main/java/com/github/klepus/service/menu/MenuService.java
+++ src/main/java/com/github/klepus/service/menu/MenuService.java
@@ -1,12 +1,13 @@
 package com.github.klepus.service.menu;
 
 
+import com.github.klepus.model.Menu;
 import com.github.klepus.to.menu.MenuTo;
 
 import java.time.LocalDate;
 
 public interface MenuService {
-    void create(MenuTo menu);
+    Menu create(MenuTo menu);
 
     void update(MenuTo menu);
 
