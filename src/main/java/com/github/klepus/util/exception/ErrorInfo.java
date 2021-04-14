e7cd107cedebe34d987b994b70d7a83606a07d07
Aleks Klepinin <aleks.klepinin@gmail.com>
2021-04-14 14:17:36 +0300

    Add exceptions handling

---
+++ src/main/java/com/github/klepus/util/exception/ErrorInfo.java
@@ -0,0 +1,13 @@
+package com.github.klepus.util.exception;
+
+public class ErrorInfo {
+    private final String url;
+    private final ErrorType type;
+    private final String typeMessage;
+
+    public ErrorInfo(CharSequence url, ErrorType type, String typeMessage) {
+        this.url = url.toString();
+        this.type = type;
+        this.typeMessage = typeMessage;
+    }
+}
---
+++ src/main/java/com/github/klepus/web/ExceptionInfoHandler.java
@@ -0,0 +1,54 @@
+package com.github.klepus.web;
+
+import com.github.klepus.util.ValidationUtil;
+import com.github.klepus.util.exception.ErrorInfo;
+import com.github.klepus.util.exception.ErrorType;
+import com.github.klepus.util.exception.NotFoundException;
+import com.github.klepus.util.exception.VotingException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static com.github.klepus.util.exception.ErrorType.*;
+
+@RestControllerAdvice(annotations = RestController.class)
+@Order(Ordered.HIGHEST_PRECEDENCE + 5)
+public class ExceptionInfoHandler {
+    private static Logger log = LoggerFactory.getLogger(ExceptionInfoHandler.class);
+
+    @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
+    @ExceptionHandler(VotingException.class)
+    public ErrorInfo votingError(HttpServletRequest req, VotingException e) {
+        return logAndGetErrorInfo(req, e, false, DATA_ERROR);
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ErrorInfo notFoundError(HttpServletRequest req, NotFoundException e) {
+        return logAndGetErrorInfo(req, e, true, DATA_NOT_FOUND);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public ErrorInfo handleError(HttpServletRequest req, Exception e) {
+        return logAndGetErrorInfo(req, e, true, APP_ERROR);
+    }
+
+    private ErrorInfo logAndGetErrorInfo(HttpServletRequest req, Exception e, boolean logException, ErrorType errorType) {
+        Throwable rootCause = ValidationUtil.getRootCause(e);
+        if (logException) {
+            log.error(errorType + " at request " + req.getRequestURL(), rootCause);
+        } else {
+            log.warn("{} at request  {}: {}", errorType, req.getRequestURL(), rootCause.toString());
+        }
+        return new ErrorInfo(req.getRequestURL(), errorType, rootCause.toString());
+    }
+}
